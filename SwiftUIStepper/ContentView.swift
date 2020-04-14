000b04d6f2a2025689603242ed85acc290ba8941
Sophia <gmb728@gmail.com>
2020-04-14 08:31:30 +0800

    Initial Commit

---
+++ SwiftUIStepper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIStepper
+//
+//  Created by Chang Sophia on 4/14/20.
+//  Copyright © 2020 Chang Sophia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
